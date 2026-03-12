f87526b063c0be8378762bc98591f37dc1dc3e15
vivek <vivekkrishnan1729@gmail.com>
2026-03-12 17:52:53 +0530

    Initial Commit

---
+++ MoodMac/MoodMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMacApp.swift
+//  MoodMac
+//
+//  Created by Vivek Krishnan on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
